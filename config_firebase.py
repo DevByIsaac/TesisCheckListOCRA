9683e9741d34b85f3fb4655440c11616c309cbc3
DevByIsaac <isaac_gomez_elmaster@live.com>
2024-07-05 21:47:44 -0500

    Initial Commit

---
+++ config_firebase.py
@@ -0,0 +1,10 @@
+
+config = {
+  'apiKey': "AIzaSyBDzmaXXy5zFbuHWGnYR4nlufWIQ84W5oo",
+  'authDomain': "app-detecciones.firebaseapp.com",
+  'databaseURL': "https://app-detecciones-default-rtdb.europe-west1.firebasedatabase.app",
+  'projectId': "app-detecciones",
+  'storageBucket': "app-detecciones.appspot.com",
+  'messagingSenderId': "702785959795",
+  'appId': "1:702785959795:web:f2971717fa6f8d4af0b706"
+}
